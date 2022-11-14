a5235ff977c95b46ec00636e6741e305bc392eb4
coder-monishr <112107452+coder-monishr@users.noreply.github.com>
2022-11-14 19:33:43 +0530

    Add files via upload

---
+++ arrays.c
@@ -0,0 +1,13 @@
+// this is a program I do while learning arrays in C language 
+
+#include  <stdio.h>
+
+int main(){
+    int myNUm[] = {2,4,5,7,8,86};
+    //wanna access it, you can access it through the index numbers of the array
+    //unlike other languages it's index number also starts from 0
+    printf("%d \n", myNUm[0]);
+    printf("%d is the sum of 1st and second element! ", myNUm[0] + myNUm[1]);
+
+    // we can also change the element in a array
+}
